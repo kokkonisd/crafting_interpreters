ea3c41067882cd0c629c4f33df371563384cc4b6
Dimitris Kokkonis <kokkonisd@gmail.com>
2022-02-28 08:57:45 +0100

    Make "init" return "this"
    The constructor/initialization method of a class should always return
    "this". Some corner cases (such as a plain `return;` or a `return
    <something>;`) must be accounted for as well.

--- jlox/src/Resolver.java
+++ jlox/src/Resolver.java
@@ -19,6 +19,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private enum FunctionType {
         NONE,
         FUNCTION,
+        INITIALIZER,
         METHOD
     }
 
@@ -159,7 +160,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
             Lox.error(stmt.keyword, "Can't return from top-level code.");
         }
 
-        if (stmt.value != null) resolve(stmt.value);
+        if (stmt.value != null) {
+            if (currentFunction == FunctionType.INITIALIZER) {
+                Lox.error(stmt.keyword, "Can't return a value from an initializer.");
+            }
+            resolve(stmt.value);
+        }
         return null;
     }
 
@@ -250,6 +256,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         // Resolve class methods.
         for (Stmt.Function method : stmt.methods) {
             FunctionType declaration = FunctionType.METHOD;
+            if (method.name.lexeme.equals("init")) {
+                declaration = FunctionType.INITIALIZER;
+            }
             resolveFunction(method, declaration);
         }
 
