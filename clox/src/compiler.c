e13432c00ab3c2fdb0e4629e42d0dc3ae736eade
Dimitris Kokkonis <kokkonisd@gmail.com>
2022-04-10 12:44:31 +0200

    Add support for if/else, while, for

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -153,6 +153,35 @@ static void emitBytes (uint8_t byte1, uint8_t byte2)
 }
 
 
+static void emitLoop (int loopStart)
+{
+    emitByte(OP_LOOP);
+
+    // +2 because we also need to jump over the operands of the OP_LOOP instruction.
+    int offset = currentChunk()->count - loopStart + 2;
+    if (offset > UINT16_MAX) error("Loop body too large.");
+
+    emitByte((offset >> 8) & 0xff);
+    emitByte(offset & 0xff);
+}
+
+
+static int emitJump (uint8_t instruction)
+{
+    emitByte(instruction);
+    // Emit a placeholder for the jump address; we'll come back to patch this via
+    // `patchJump()` after we have computed the address to jump to.
+    //
+    // Do note that this architecture choice means that we can jump over up to
+    // 65535 bytes of code, so there is a hard limit (and an edge case to be found
+    // here).
+    emitByte(0xff);
+    emitByte(0xff);
+
+    return currentChunk()->count - 2;
+}
+
+
 static void emitReturn ()
 {
     emitByte(OP_RETURN);
@@ -174,6 +203,21 @@ static uint8_t makeConstant (Value value)
 static void emitConstant (Value value)
 {
     emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+
+static void patchJump (int offset)
+{
+    // -2 to adjust for the bytecode for the jump offset itself.
+    int jump = currentChunk()->count - offset - 2;
+
+    if (jump > UINT16_MAX) {
+        error("Too much code to jump over.");
+    }
+
+    // Split the jump address into a low byte and a high byte.
+    currentChunk()->code[offset] = (jump >> 8) & 0xff;
+    currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
 
@@ -315,6 +359,36 @@ static void markInitialized ()
 }
 
 
+static void defineVariable (uint8_t global)
+{
+    // If it's not a global variable, then we just put it on the stack.
+    // There is nothing else to do here; no instruction to emit, so we return.
+    if (current->scopeDepth > 0) {
+        markInitialized();
+        return;
+    }
+
+    emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
+
+static void and_ (bool canAssign)
+{
+    // At this point, the left-hand expression has been evaluated and put on the stack.
+    // This menas that, if the left-hand expression is false, we can already
+    // short-circuit the `and` operator and just return `false` immediately.
+    // There is no need to evaluate the right-hand expression, so we can jump over it.
+    // The result of the `and` operation will be on the stack when we return, as the
+    // left-hand operand is already on the stack.
+    int endJump = emitJump(OP_JUMP_IF_FALSE);
+
+    emitByte(OP_POP);
+    parsePrecedence(PREC_AND);
+
+    patchJump(endJump);
+}
+
+
 static void binary (bool canAssign)
 {
     TokenType operatorType = parser.previous.type;
@@ -359,6 +433,21 @@ static void number (bool canAssign)
 {
     double value = strtod(parser.previous.start, NULL);
     emitConstant(NUMBER_VAL(value));
+}
+
+
+static void or_ (bool canAssign)
+{
+    // Same as in `and_()`, the reason we add jump statements here is to short-circuit
+    // the "or" operator in case the left-hand value is true.
+    int elseJump = emitJump(OP_JUMP_IF_FALSE);
+    int endJump = emitJump(OP_JUMP);
+
+    patchJump(elseJump);
+    emitByte(OP_POP);
+
+    parsePrecedence(PREC_OR);
+    patchJump(endJump);
 }
 
 
@@ -438,7 +527,7 @@ ParseRule rules[] = {
     [TOKEN_IDENTIFIER]    = {variable, NULL,   PREC_NONE},
     [TOKEN_STRING]        = {string,   NULL,   PREC_NONE},
     [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
-    [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_AND]           = {NULL,     and_,   PREC_AND},
     [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
     [TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE},
     [TOKEN_FALSE]         = {literal,  NULL,   PREC_NONE},
@@ -446,7 +535,7 @@ ParseRule rules[] = {
     [TOKEN_FUN]           = {NULL,     NULL,   PREC_NONE},
     [TOKEN_IF]            = {NULL,     NULL,   PREC_NONE},
     [TOKEN_NIL]           = {literal,  NULL,   PREC_NONE},
-    [TOKEN_OR]            = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_OR]            = {NULL,     or_,    PREC_OR},
     [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
     [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
     [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
@@ -488,17 +577,6 @@ static void parsePrecedence (Precedence precedence)
 }
 
 
-static void defineVariable (uint8_t global)
-{
-    // If it's not a global variable, then we just put it on the stack.
-    // There is nothing else to do here; no instruction to emit, so we return.
-    if (current->scopeDepth > 0) {
-        markInitialized();
-        return;
-    }
-
-    emitBytes(OP_DEFINE_GLOBAL, global);
-}
 
 
 static ParseRule * getRule (TokenType type)
@@ -550,11 +628,108 @@ static void expressionStatement ()
 }
 
 
+static void forStatement ()
+{
+    // For-statements are scoped.
+    beginScope();
+
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+    if (match(TOKEN_SEMICOLON)) {
+        // No initializer.
+    } else if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else {
+        expressionStatement();
+    }
+
+    int loopStart = currentChunk()->count;
+    int exitJump = -1;
+
+    if (!match(TOKEN_SEMICOLON)) {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+        // Jump out of the loop if the condition is false.
+        exitJump = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP); // Condition.
+    }
+
+    // We now need to manage the increment clause.
+    // Since we're making a single pass over the code, we'll jump over the increment,
+    // run the body, jump back up to the increment, run it, and then go to the next
+    // iteration.
+    if (!match(TOKEN_RIGHT_PAREN)) {
+        int bodyJump = emitJump(OP_JUMP);
+        int incrementStart = currentChunk()->count;
+
+        expression();
+        emitByte(OP_POP); // Pop the result of the expression off the stack.
+
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
+
+    statement();
+    emitLoop(loopStart);
+
+    if (exitJump != -1) {
+        patchJump(exitJump);
+        emitByte(OP_POP); // Condition.
+    }
+
+    endScope();
+}
+
+
+static void ifStatement ()
+{
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+    int thenJump = emitJump(OP_JUMP_IF_FALSE);
+    // Pop the condition off the stack (if the condition is truthy).
+    emitByte(OP_POP);
+    statement();
+
+    int elseJump = emitJump(OP_JUMP);
+
+    patchJump(thenJump);
+    // Pop the condition off the stack (if the condition is falsey).
+    emitByte(OP_POP);
+
+    if (match(TOKEN_ELSE)) statement();
+    patchJump(elseJump);
+}
+
+
 static void printStatement ()
 {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
     emitByte(OP_PRINT);
+}
+
+
+static void whileStatement ()
+{
+    int loopStart = currentChunk()->count;
+
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+    int exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    statement();
+
+    emitLoop(loopStart);
+
+    patchJump(exitJump);
+    emitByte(OP_POP);
 }
 
 
@@ -601,6 +776,12 @@ static void statement ()
 {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else if (match(TOKEN_FOR)) {
+        forStatement();
+    } else if (match(TOKEN_IF)) {
+        ifStatement();
+    } else if (match(TOKEN_WHILE)) {
+        whileStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
         beginScope();
         block();
--- clox/src/vm.c
+++ clox/src/vm.c
@@ -98,6 +98,7 @@ static InterpretResult run ()
 {
 #   define READ_BYTE() (*vm.ip++)
 #   define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#   define READ_SHORT() (vm.ip += 2, (uint16_t) ((vm.ip[-2] << 8) | vm.ip[-1]))
 #   define READ_STRING() AS_STRING(READ_CONSTANT())
 // This is a macro hack to make sure this macro works everywhere.
 // We want to make sure all instructions share the same scope; you might think we could
@@ -117,7 +118,7 @@ static InterpretResult run ()
 
     for (;;) {
 #       ifdef DEBUG_TRACE_EXECUTION
-            printf("          ");
+            printf("STACK     ");
             for (Value * slot = vm.stack; slot < vm.stackTop; slot++) {
                 printf("[ ");
                 printValue(*slot);
@@ -223,6 +224,22 @@ static InterpretResult run ()
                 printf("\n");
                 break;
             }
+            case OP_JUMP: {
+                uint16_t offset = READ_SHORT();
+                vm.ip += offset;
+                break;
+            }
+            case OP_JUMP_IF_FALSE: {
+                uint16_t offset = READ_SHORT();
+                if (isFalsey(peek(0))) vm.ip += offset;
+                break;
+            }
+            case OP_LOOP: {
+                uint16_t offset = READ_SHORT();
+                // Loops jump backwards.
+                vm.ip -= offset;
+                break;
+            }
             case OP_RETURN: {
                 // Exit interpreter.
                 return INTERPRET_OK;
@@ -230,6 +247,7 @@ static InterpretResult run ()
         }
      }
 #    undef READ_BYTE
+#    undef READ_SHORT
 #    undef READ_CONSTANT
 #    undef READ_STRING
 #    undef BINARY_OP
